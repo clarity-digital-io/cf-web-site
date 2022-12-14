f551f4c6e04ee5c2a8f20c202855de7bf40058db
Benjamin Efigenio <befigenio@gmail.com>
2022-12-14 11:03:40 +0200

    wip - update general colors

--- src/components/UI/Icons/ClarityFormsLogo.tsx
+++ src/components/UI/Icons/ClarityFormsLogo.tsx
@@ -1,5 +1,5 @@
 import * as React from "react";
-const ClarityFormsLogo = (props) => (
+const ClarityFormsLogo = (props: any) => (
   <svg
     xmlns="http://www.w3.org/2000/svg"
     xmlnsXlink="http://www.w3.org/1999/xlink"
